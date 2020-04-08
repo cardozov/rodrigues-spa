deb5d5798a6826982a9acd0197f8de94192c9625
Vinicius G Cardozo <vinicius.cardozo@Viniciuss-MacBook-Pro.local>
2020-04-08 13:40:09 -0300

    [ADD] router system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
 
 import Calendar from './Components/Calendar/Calendar'
+import NotFound from './Components/404/404'
 
 const App = () => (
-  <div className="App">
-    <Calendar />
-  </div>
+  <main>
+    <div>
+      <Link to="/">Home </Link>
+      <Link to="/calendar">Agenda </Link>
+      <Link to="/list">Listagem </Link>
+    </div>
+
+    <Switch>
+      <Route path="/" component={Calendar} exact/>
+      <Route path="/calendar" component={Calendar} />
+      <Route component={NotFound} />
+    </Switch>
+  </main>
 )
 
 export default App
---
+++ src/Components/404/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const page404 = () => (
+  <h1>Esta página não foi encontrada</h1>
+)
+
+export default page404
